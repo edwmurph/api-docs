2daf4e445b48fb97403718fb3cbd457544997ae0
edwmurph <20996513+edwmurph@users.noreply.github.com>
2023-07-01 08:36:59 -0400

    add vite + vue client side rendered frontend

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+  extends: [
+    '@edwmurph/eslint-config/vue'
+  ]
+};
---
+++ client/app.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <div>success!</div>
+  </main>
+</template>
+
+<style>
+  body {
+    margin: 0;
+  }
+</style>
