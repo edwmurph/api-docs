622cac7658d088c9e4b50417de830ea3088cda07
edwmurph <20996513+edwmurph@users.noreply.github.com>
2023-07-01 21:35:51 -0400

    use multiple urls

--- client/components/swagger-ui.jsx
+++ client/components/swagger-ui.jsx
@@ -6,6 +6,10 @@ import Logo from './logo.jsx';
 export default function SwaggerUIComponent() {
   useEffect( () => {
     SwaggerUIBundle({
+      urls: [
+        { url: '/definitions/team1/def1.json', name: 'team1/def1' },
+        { url: '/definitions/team1/def2.json', name: 'team1/def2' }
+      ],
       dom_id: '#swagger-ui',
       queryConfigEnabled: true,
       // layout: 'Layout',
