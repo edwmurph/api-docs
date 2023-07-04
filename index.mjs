778949bca1bb1e773e2788a778db6d26ed6bc871
edwmurph <20996513+edwmurph@users.noreply.github.com>
2023-07-03 22:47:00 -0400

    update routes to fetch definitions from s3

--- index.mjs
+++ index.mjs
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import app from './lib/app.mjs';
 
 app.start().catch( console.error.bind('error starting app:') );
--- tf/s3.tf
+++ tf/s3.tf
@@ -28,10 +28,18 @@ resource "aws_s3_bucket_acl" "definitions" {
   acl    = "public-read"
 }
 
-resource "aws_s3_object" "object" {
+resource "aws_s3_object" "team1_webhook_example" {
   bucket       = aws_s3_bucket.definitions.id
   key          = "team1/webhook-example.yaml"
   source       = "../definitions/team1/webhook-example.yaml"
   content_type = "text/yaml"
   etag         = filemd5("../definitions/team1/webhook-example.yaml")
+}
+
+resource "aws_s3_object" "uber_api" {
+  bucket       = aws_s3_bucket.definitions.id
+  key          = "uber/api.json"
+  source       = "../definitions/uber/api.json"
+  content_type = "application/json"
+  etag         = filemd5("../definitions/uber/api.json")
 }
