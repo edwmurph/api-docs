e428f4089a7ea7079fb4261cae42085cf0661c80
edwmurph <20996513+edwmurph@users.noreply.github.com>
2023-07-13 15:14:56 -0400

    dynamically fetch + log definition

--- client/components/definition.jsx
+++ client/components/definition.jsx
@@ -1,8 +1,22 @@
 import { useParams } from 'react-router-dom';
+import use_api from '../hooks/use-api';
 
 export default function Definition() {
   const params = useParams();
+
   const definition = params?.['*'];
 
-  return ( <div>{definition}</div> );
+  const { data, loading, error } = use_api({ route: `/api/definition?path=${ definition }` });
+
+  if ( error ) {
+    return error;
+  }
+
+  if ( loading ) {
+    return 'Loading...';
+  }
+
+  console.log( data );
+
+  return ( <div>done</div> );
 }
--- index.mjs
+++ index.mjs
@@ -1,4 +1,4 @@
 import 'dotenv/config';
-import app from './lib/app.mjs';
+import app from './server/app.mjs';
 
 app.start().catch( console.error.bind('error starting app:') );
